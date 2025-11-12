5fb0989b597d465430a4704df12d6c8f55fbaa35
AalokeCode <cuba2419@gmail.com>
2025-11-12 17:22:34 +0530

    fixed authentication

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  console.log("Body:", req.body);
+  next();
+});
 
 app.use("/auth", authRoutes);
 
